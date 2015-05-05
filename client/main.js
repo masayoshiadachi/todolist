0c944813b89ca80678c7f99bcfe06bdc0b938ba7
posisapi <masayoshi.adachi@gmail.com>
2015-05-05 14:24:02 +0900

    added create function

--- client/main.js
+++ client/main.js
@@ -8,9 +8,30 @@ var Tasks = Backbone.Collection.extend({
 	url : '/task'
 });
 
-var tasks = new Tasks();
-tasks.fetch().done(function() {
-	tasks.each(function(task) {
-		$('<li>').text(task.get('title')).appendTo($('ul#tasks'));
-	});
+var TasksView = Backbone.View.extend({
+	events : {
+		'click button#create' : 'onCreate',
+	},
+	initialize : function() {
+		this.listenTo(this.collection, 'add', this.onAdd.bind(this));
+	},
+	onAdd : function(task) {
+		$('<li>').text(task.get('title')).appendTo(this.$('ul'));
+	},
+	onCreate : function() {
+		var task = new Task({
+			title : this.$('input').val()
+		});
+		task.save().done(function() {
+			this.collection.add(task);
+			this.$('input').val('');
+		}.bind(this));
+	},
 });
+
+var tasks = new Tasks();
+var tasksView = new TasksView({
+	el : 'div#tasks',
+	collection : tasks,
+});
+tasks.fetch();
